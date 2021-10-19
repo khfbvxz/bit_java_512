c715ca9f3693d58d7cca729b4b387940a36cad1e
YuHwan Kim <khfbvxz@naver.com>
2021-10-19 16:34:19 +0900

    삭제, 전체레코드 출력

--- Test1019/src/net/bit/day19/DB.java
+++ Test1019/src/net/bit/day19/DB.java
@@ -1,10 +1,23 @@
 package net.bit.day19;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 public class DB {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+//	private Connection CN =null;
+//	private Statement ST = null;
+	public static Connection getConnection() {
+		Connection CN =null;
+		Statement ST = null;
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
+			CN = DriverManager.getConnection(url, "system", "1234");
+			ST = CN.createStatement();
+		} catch (Exception e) { 
+			System.out.println("DB서버 연결 실패 "+ e.toString());
+		}
+		return CN;
 	}
 
 }
--- Test1019/src/net/bit/day19/DBTest.java
+++ Test1019/src/net/bit/day19/DBTest.java
@@ -5,95 +5,130 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class DBTest {
-	public static void main(String[] args) {
-		Connection CN = null; // DB서버주소및 id,pwd 정보를 CN기억하고 명령어생성할때 참조
-		Statement ST = null; // 정적인명령어
-		ResultSet RS = null; // 조회한결과기억 RS = ST.executeQuery("select"); while(RS.next()){ }
-		String msg = "insert,delete,update,select";
-		int codeA = 0, hitD = 79;
-		String nameB = "";
 
+	private Connection CN = null; // DB서버주소및 id,pwd 정보를 CN기억하고 명령어생성할때 참조
+	private Statement ST = null; // 정적인명령어
+	private ResultSet RS = null; // 조회한결과기억 RS = ST.executeQuery("select"); while(RS.next()){ }
+	private String msg, Gmsg, nameB;
+	private int Gtotal, codeA, hitD;
+
+	public DBTest() {
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-			CN = DriverManager.getConnection(url, "system", "1234");
+			CN = DB.getConnection();
 			ST = CN.createStatement();
-			dbSelectAll();
+		} catch (Exception ex) {
+			System.out.println("에러이유 " + ex);
+		}
+	}// 기본 생성자.
 
-			// 코드,이름 입력하고 날짜하고hit기본값으로
-			Scanner sc = new Scanner(System.in);
-			// 강사님 
-			while(true) {
-				System.out.print("코드code입력 >>> ");
-				codeA = Integer.parseInt(sc.nextLine());
-				msg = "select count(*) as cnt from test where code =" + codeA;
-				RS = ST.executeQuery(msg);
-				if(RS.next()==true) {
-					int total = RS.getInt("cnt");
-					if(total>0) {
-						System.out.println(codeA+" 데이터는 중복된 데이터입니다.\n ");
-						continue;
-					}
-					break;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			DBTest ob = new DBTest();
+			ob.dbSelectAll();
+//			ob.dbInsert();
+			while (true) {
+				System.out.print("1.신규등록  2.전체 출력  3.삭제 4.수정  5.이름검색  9.종료 >>>");
+				int sel = Integer.parseInt(sc.nextLine());
+
+				if (sel == 9) {
+					System.out.println("test 데이블 crud 처리를 종료합니다.");
+					System.exit(1);
+				} else if (sel == 1) {
+					ob.dbInsert();
+				} else if (sel == 2) {
+					ob.dbSelectAll();
+				} else if (sel == 3) {
+					ob.dbDelete();
+				} else {
+					System.out.println("숫자를 제대로 입력하세요");
 				}
-			}//while end
-//			boolean codecheck = true;
-//			while (codecheck) {
-//				System.out.print("코드code입력 >>> ");
-//				codeA = Integer.parseInt(sc.nextLine());
-//				// 코드 중복 체크
-//				msg = "select count(*) as cnt from test where code =" + codeA;
-//				RS = ST.executeQuery(msg);
-//				RS.next();
-////	       System.out.println(RS.next());
-//				int a = 0;
-//				a = RS.getInt("cnt");
-//				System.out.println("a  " + a);
-//				if (a == 1) {
-//					System.out.println("코드가 중복되었습니다.1");
-//					System.out.println("다시 입력하세요");
-//					codecheck = true;
-//				} else {
-//					System.out.println("중복이 없습니다.");
-//					codecheck = false;
-//				}
-//			}
-
-			System.out.print("이름name입력 >>> ");
-			nameB = sc.nextLine();
-
-			msg = "insert into test(code,name,wdate,hit) values( " + codeA + ", '" + nameB + "', sysdate, " + hitD
-					+ ")";
-			System.out.println(msg);
-			int OK = ST.executeUpdate(msg); // 진짜 실행
-			if (OK > 0) {
-				System.out.println(codeA + "데이터 저장 성공했습니다.");
-			} else {
-				System.out.println(codeA + "데이터 저장 실패했습니다.");
 			}
-			dbSelectAll();
+
 		} catch (Exception ex) {
 			System.out.println("에러이유 " + ex);
 		}
 	}// main end
 
-	public static void dbSelectAll() {
-		// 다시 한번 전체 출력
-		Connection CN = null; // DB서버주소및 id,pwd 정보를 CN기억하고 명령어생성할때 참조
-		Statement ST = null; // 정적인명령어
-		ResultSet RS = null; // 조회한결과기억 RS = ST.executeQuery("select"); while(RS.next()){ }
-		String msg = "insert,delete,update,select";
+	public void dbDelete() { // non -static
+		// 코드 code 필드 삭제
+		try {
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				System.out.print("삭제할 code 입력 >>> ");
+				codeA = Integer.parseInt(sc.nextLine());
+				msg = "select count(*) as cnt from test where code = " + codeA;
+				RS = ST.executeQuery(msg);
+				if (RS.next() == true) {
+					int total = RS.getInt("cnt");
+					System.out.println();
+					if (total == 0) {
+						System.out.println(codeA + " 데이터는 없는 데이터입니다\n ");
+						continue;
+					}
+					break;
+				}
+			}
+			msg = "delete from test where code =" + codeA;
+			RS = ST.executeQuery(msg);
+
+			int DL = ST.executeUpdate(msg); // 되면 반환 0
+			System.out.println(DL);
+			if (DL == 0) { // code가 없어도 실행됨..
+				System.out.println(codeA + "데이터 삭제 성공!");
+			} else {
+				System.out.println(codeA + "데이터 삭제 실패!");
+			}
+		} catch (Exception ex) {
+			System.out.println("에러이유" + ex);
+		}
+	}// dbDelete() end
+
+	public void dbInsert() {
+		try {
+			Scanner sc = new Scanner(System.in);
+			while (true) {
+				System.out.print("코드code입력 >>> ");
+				codeA = Integer.parseInt(sc.nextLine());
+				msg = "select count(*) as cnt from test where code = " + codeA;
+				RS = ST.executeQuery(msg);
+				if (RS.next() == true) {
+					int total = RS.getInt("cnt");
+					if (total > 0) {
+						System.out.println(codeA + " 데이터는 중복된 데이터입니다\n ");
+						continue;
+					}
+					break;
+				}
+			} // while end
+
+			System.out.print("이름name입력 >>> ");
+			nameB = sc.nextLine();
+
+			msg = "insert into test(code,name,wdate,hit) values(" + codeA + ",'" + nameB + "', sysdate," + hitD + ")";
+			int OK = ST.executeUpdate(msg); // 진짜실행
+			if (OK > 0) {
+				System.out.println(codeA + " 데이터저장 성공했습니다");
+				this.dbSelectAll();
+			} else {
+				System.out.println(codeA + " 데이터저장 실패했습니다");
+			}
+		} catch (Exception ex) {
+			System.out.println("에러이유 " + ex);
+		}
+	}// dbInsert() end
+
+	public void dbSelectAll() {
 
 		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@127.0.0.1:1521:XE";
-			CN = DriverManager.getConnection(url, "system", "1234");
-
-			ST = CN.createStatement(); // 명령어 한번 생성후 재활용
+			msg = "select count(*) as cnt from test";
+			RS = ST.executeQuery(msg);
+			RS.next();
+			Gtotal = RS.getInt("cnt");
+			System.out.println("\t\t전체레코드갯수 : " + Gtotal);
 
 			msg = "select code,name,wdate,hit from test order by code asc";
 			RS = ST.executeQuery(msg); // 조회한결과 전체를 RS기억
-
 			System.out.println("\n코드\t 이름\t 날짜\t조회수");
 			System.out.println("-------------------------------");
 			while (RS.next() == true) {
